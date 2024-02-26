032d4511e659fd2a65d0c644ca950eabe48fc088
DimaTrikh <d.trikhunkov@outlook.com>
2024-02-26 13:40:46 +0300

    вроде бы пришло осознание

--- parent.cs
+++ parent.cs
@@ -1,14 +1,26 @@
 abstract class Parent
 {
     protected static Random rnd = new Random();
-    protected bool fill_rand = false;
     protected Parent(bool fill_rand = false)
     {
         this.fill_rand = fill_rand;
     }
-    public abstract void Rand();
 
-    public abstract void Manual();
+    public virtual void ReCreate(bool fill_rand = false)
+        {
+            if (fill_rand)
+            {
+                Manual();
+            }
+            else
+            {
+                Rand();
+            }
+        }
+
+    protected abstract void Rand();
+
+    protected abstract void Manual();
     
     public abstract void Print();
     
